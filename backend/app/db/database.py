033ba07ac64e0dd50898e5514f5baec81174180a
YuriBre <brenoyuri5000@gmail.com>
2025-06-26 15:44:00 -0300

    chore: muda a estrutura de pastas

--- backend/app/db/database.py
+++
@@ -1,5 +0,0 @@
-from databases import Database
-
-DATABASE_URL = "mysql+aiomysql://user:user123@db/mercado"
-
-database = Database(DATABASE_URL)
--- backend/app/views/produto_view.py
+++
@@ -1,5 +0,0 @@
-from fastapi import APIRouter
-from app.controllers.produto_controller import router as produto_router
-
-router = APIRouter()
-router.include_router(produto_router, prefix="/produtos", tags=["Produtos"])
