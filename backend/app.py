8038e06122d3072b90cd32114bf6ea87482070e8
Breno Yuri <brenoyuri5000@gmail.com>
2025-07-17 16:20:46 -0300

    feat(api): configura app principal

--- backend/app.py
+++ backend/app.py
@@ -1,15 +1,18 @@
 from fastapi import FastAPI
+from database import Base, engine
 from cliente.router import router as clientes
 from produto.router import router as produtos
 from venda.router import router as vendas
-from pagamento.router import router as pagamentos
+from vendedor.router import router as vendedores
 
 app = FastAPI()
+
+Base.metadata.create_all(bind=engine)
 
 app.include_router(clientes, prefix="/clientes", tags=["Clientes"])
 app.include_router(produtos, prefix="/produtos", tags=["Produtos"])
 app.include_router(vendas, prefix="/vendas", tags=["Vendas"])
-app.include_router(pagamentos, prefix="/pagamentos", tags=["Pagamentos"])
+app.include_router(vendedores, prefix="/vendedores", tags=["Vendedores"])
 
 @app.get("/")
 def root():
