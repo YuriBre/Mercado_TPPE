380f491f68166b24bb084446be66add304fde3a6
Breno Yuri <brenoyuri5000@gmail.com>
2025-07-17 16:20:46 -0300

    feat(api): atualiza model da entidade venda

--- backend/venda/model.py
+++ backend/venda/model.py
@@ -1,11 +1,17 @@
-from sqlalchemy import Column, Integer, Float, ForeignKey
+from sqlalchemy import Column, Integer, Float, String, ForeignKey
 from database import Base
+from sqlalchemy.orm import relationship
 
 class ModeloVenda(Base):
     __tablename__ = "venda"
 
     id = Column(Integer, primary_key=True, index=True)
-    cliente_id = Column(Integer, ForeignKey("cliente.id"), nullable=False)
+    cliente_cpf = Column(String, ForeignKey("cliente.cpf"), nullable=False)
+    vendedor_cpf = Column(String, ForeignKey("vendedor.cpf"), nullable=False)
     produto_id = Column(Integer, ForeignKey("produto.id"), nullable=False)
-    quantidade = Column(Integer, nullable=False)
+    quantidade = Column(Integer, nullable=False, default=1)
     valor_total = Column(Float, nullable=False, default=0.00)
+
+    cliente = relationship("ModeloCliente", backref="vendas")
+    vendedor = relationship("ModeloVendedor", backref="vendas")
+    produto = relationship("ModeloProduto", backref="vendas")
