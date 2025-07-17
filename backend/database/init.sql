e062363c35294e2967c54f2dfe9d222c7b414c28
Breno Yuri <brenoyuri5000@gmail.com>
2025-07-17 16:20:46 -0300

    feat(api): adapta init.sql para PostgreSQL

--- backend/database/init.sql
+++ backend/database/init.sql
@@ -1,37 +1,35 @@
-CREATE DATABASE IF NOT EXISTS mercado;
-USE mercado;
 -- Tabela Cliente
 CREATE TABLE IF NOT EXISTS cliente (
-    id INT AUTO_INCREMENT PRIMARY KEY,
+    cpf VARCHAR(11) PRIMARY KEY,
     nome VARCHAR(100) NOT NULL,
     endereco VARCHAR(150) NOT NULL,
     telefone VARCHAR(20) NOT NULL
 );
+-- Tabela Vendedor
+CREATE TABLE IF NOT EXISTS vendedor (
+    cpf VARCHAR(11) PRIMARY KEY,
+    nome VARCHAR(100) NOT NULL,
+    email VARCHAR(100) NOT NULL,
+    telefone VARCHAR(20) NOT NULL
+);
 -- Tabela Produto
 CREATE TABLE IF NOT EXISTS produto (
-    id INT AUTO_INCREMENT PRIMARY KEY,
+    id SERIAL PRIMARY KEY,
     nome VARCHAR(100) NOT NULL,
     descricao VARCHAR(255) NOT NULL,
-    valor DECIMAL(10, 2) NOT NULL,
-    lucro_percentual DECIMAL(5, 2) NOT NULL,
-    qtd_estoque INT NOT NULL
+    valor NUMERIC(10, 2) NOT NULL,
+    lucro_percentual NUMERIC(5, 2) NOT NULL,
+    qtd_estoque INTEGER NOT NULL
 );
 -- Tabela Venda
 CREATE TABLE IF NOT EXISTS venda (
-    id INT AUTO_INCREMENT PRIMARY KEY,
-    cliente_id INT NOT NULL,
-    produto_id INT NOT NULL,
-    quantidade INT NOT NULL,
-    valor_total DECIMAL(10, 2) NOT NULL,
-    FOREIGN KEY (cliente_id) REFERENCES cliente(id),
-    FOREIGN KEY (produto_id) REFERENCES produto(id)
-);
--- Tabela Pagamento
-CREATE TABLE IF NOT EXISTS pagamento (
-    id INT AUTO_INCREMENT PRIMARY KEY,
-    venda_id INT NOT NULL,
-    valor_pago DECIMAL(10, 2) NOT NULL,
-    metodo_pagamento ENUM('dinheiro', 'cartao', 'pix', 'boleto') NOT NULL,
-    data_pagamento DATETIME DEFAULT CURRENT_TIMESTAMP,
-    FOREIGN KEY (venda_id) REFERENCES venda(id) ON DELETE CASCADE
+    id SERIAL PRIMARY KEY,
+    cliente_cpf VARCHAR(11) NOT NULL,
+    produto_id INTEGER NOT NULL,
+    vendedor_cpf VARCHAR(11) NOT NULL,
+    quantidade INTEGER NOT NULL,
+    valor_total NUMERIC(10, 2) NOT NULL,
+    FOREIGN KEY (cliente_cpf) REFERENCES cliente(cpf),
+    FOREIGN KEY (produto_id) REFERENCES produto(id),
+    FOREIGN KEY (vendedor_cpf) REFERENCES vendedor(cpf)
 );
