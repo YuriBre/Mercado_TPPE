eeabf474b80451159652363951174d61fc05be34
Breno Yuri <brenoyuri5000@gmail.com>
2025-07-17 16:20:46 -0300

    feat(api): atualiza control da entidade cliente

--- backend/cliente/control.py
+++ backend/cliente/control.py
@@ -8,22 +8,22 @@ class ControleCliente:
     def adicionar_cliente(self, cliente):
         self.clientes.append(cliente)
 
-    def buscar_cliente_por_id(self, id):
+    def buscar_cliente_por_cpf(self, cpf):
         for cliente in self.clientes:
-            if cliente.get_id() == id:
+            if cliente.get_cpf() == cpf:
                 return cliente
         return None
 
-    def atualizar_cliente(self, id, novos_dados):
-        cliente = self.buscar_cliente_por_id(id)
+    def atualizar_cliente(self, cpf, novos_dados):
+        cliente = self.buscar_cliente_por_cpf(cpf)
         if cliente:
             for chave, valor in novos_dados.items():
                 setattr(cliente, chave, valor)
             return cliente
         return None
 
-    def deletar_cliente(self, id):
-        cliente = self.buscar_cliente_por_id(id)
+    def deletar_cliente(self, cpf):
+        cliente = self.buscar_cliente_por_cpf(cpf)
         if cliente:
             self.clientes.remove(cliente)
             return True
