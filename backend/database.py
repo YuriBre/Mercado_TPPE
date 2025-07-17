32cb48047482e4730c58cccfd2193ae2e5264377
Breno Yuri <brenoyuri5000@gmail.com>
2025-07-17 16:20:46 -0300

    feat(api): ajusta conexão com banco de dados PostgreSQL

--- backend/database.py
+++ backend/database.py
@@ -2,7 +2,7 @@ from sqlalchemy import create_engine
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
 
-DATABASE_URL = "mysql+pymysql://root:root@database/mercado"
+DATABASE_URL = "postgresql://mercado_jghe_user:NuB8IqLQg4PyThnG8jPQhNMlfVw6KE2T@dpg-d1sis215pdvs7397dr10-a.oregon-postgres.render.com:5432/mercado_jghe?sslmode=require"
 
 engine = create_engine(DATABASE_URL)
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
