de07a6bb35fe428335a17397d1cf5ea968209d12
Breno Yuri <brenoyuri5000@gmail.com>
2025-07-17 16:20:46 -0300

    feat(api): atualiza schemas da entidade venda

--- backend/venda/schemas.py
+++ backend/venda/schemas.py
@@ -1,9 +1,14 @@
 from pydantic import BaseModel
 from typing import Optional
+from datetime import date
+from cliente.schemas import ClienteResponse
+from produto.schemas import ProdutoResponse
+from vendedor.schemas import VendedorResponse
 
 class VendaBase(BaseModel):
     id: Optional[int] = None
-    cliente_id: int
+    cliente_cpf: str
+    vendedor_cpf: str
     produto_id: int
     quantidade: int
     valor_total: Optional[float] = None
@@ -12,9 +17,24 @@ class VendaBase(BaseModel):
         orm_mode = True
 
 class VendaCreate(BaseModel):
-    cliente_id: int
+    cliente_cpf: str
+    vendedor_cpf: str
     produto_id: int
     quantidade: int
+
+    class Config:
+        orm_mode = True
+
+
+class VendaResponse(BaseModel):
+    id: int
+    data: date
+    quantidade: int
+    total: float
+    comissao: float
+    cliente: ClienteResponse
+    produto: ProdutoResponse
+    vendedor: VendedorResponse
 
     class Config:
         orm_mode = True
